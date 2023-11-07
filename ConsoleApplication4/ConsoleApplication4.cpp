5b8a001f7a56c453566c14d5d206d47f764b3916
BicepsCrazy <149767792+BicepsCrazy@users.noreply.github.com>
2023-11-07 16:11:41 +0300

    Add files via upload

---
+++ ConsoleApplication4/ConsoleApplication4.cpp
@@ -0,0 +1,31 @@
+﻿// ConsoleApplication4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Ru");
+    int num1;
+    int num2;
+    int num3;
+    int num4;
+    cout << "введите первое число" << endl;
+    cin >> num1;
+    cout << "введите второе число" << endl;
+    cin >> num2;
+
+    cout << num1 << "*" << num2 << "=?";
+    num3 = num1 * num2;
+    cin >> num4;
+    if (num4 == num3) {
+        cout << "это правильный ответ" << endl;
+
+
+    }
+    else {
+        cout << "это неправильный ответ =( " << endl;
+        cout << "правильный ответ: " << num1 * num2;
+        
+    }
+}
---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,29 @@
+﻿// ConsoleApplication5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+    const int size = 10;
+        int arr[size];
+        int sum = 0;
+
+       cout << "Введите элементы массива:" << endl;
+        for (int i = 0; i < size; i++) {
+           cin >> arr[i];
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (arr[i] % 2 != 0) {
+                sum += arr[i];
+            }       
+        }
+
+       cout << "Сумма нечетных чисел в массиве: " << sum << endl;
+
+        return 0;
+    }
